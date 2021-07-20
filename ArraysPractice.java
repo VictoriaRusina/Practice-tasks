a98ffab310bfb0f1122c7095c747239eb06c133c
VictoriaRusina <86785430+VictoriaRusina@users.noreply.github.com>
2021-07-20 12:56:42 +0300

    Add files via upload

---
+++ ArraysPractice.java
@@ -0,0 +1,69 @@
+/* Задание 2 array size
+public class ArraysPractice
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = new int[2];
+        numbers[0]=1;
+        numbers[1]=2;
+
+        int size = numbers.length;
+        System.out.println(size);
+    }
+}
+*/
+
+/* Задание 2.1 a number to search in the array
+public class ArraysPractice
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = new int[2];
+        numbers[0]=1;
+        numbers[1]=2;
+
+        System.out.println("the first number is " + numbers[0]);
+    }
+}
+ */
+
+
+
+/* test1
+public class ArraysPractice
+{ public static void main(String[] args)
+ {     int[] numbers = {1, 2};
+        for (int elem : numbers) {
+            if (elem == 3) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+
+    }
+}
+*/
+
+
+
+
+/* Задание 2.3 Generate a random array of integers of provided size and print it
+public class ArraysPractice
+{ public static void main(String[] args) {
+       int[] numbers = {1, 2, 1, -1};
+        for ( int i=0; i <= 3; i++) {
+            if (numbers [i] == 1) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+
+    }
+}
+*/
+
+//Задание 2.4 binary search
+
+
