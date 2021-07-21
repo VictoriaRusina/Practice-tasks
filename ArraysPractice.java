ce6b352d1cb5f3de252ec94b0a6e76fe963fe3db
VictoriaRusina <86785430+VictoriaRusina@users.noreply.github.com>
2021-07-21 15:59:32 +0300

    Update ArraysPractice.java

--- ArraysPractice.java
+++ ArraysPractice.java
@@ -64,6 +64,75 @@ public class ArraysPractice
 }
 */
 
+/*
 //Задание 2.4 binary search
+import java.util.Arrays;
+public class ArraysPractice
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = {1, 2, 3};
+        bubbleSort(numbers);
+    }
 
+    public static void bubbleSort(int[] numbers)
+    {
+        {
+            for (int i = 0; i < numbers.length; i++)
+            {
+                System.out.print(numbers[i] + "  ");
+            }
+            System.out.print("\n");
+
+            int index = Arrays.binarySearch(numbers, 2);
+            if(index == -1){
+                System.out.println("False");
+            } else{
+                System.out.println("True");
+            }
+        }
+    }
+}
+*/
+
+
+
+
+
+
+/* Задание 3 sort array, selection sort
+import java.util.Arrays;
+public class ArraysPractice
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = {1, 4, 3, 2};
+        selectionSort(numbers);
+    }
+    public static void selectionSort(int[] numbers)
+    {
+        for (int i = 0; i < numbers.length; i++)
+        {
+            int min = numbers[i];
+            int min_i = i;
+            for (int j = i + 1; j < numbers.length; j++)
+            {
+                if (numbers[j] < min) {
+                    min = numbers[j];
+                    min_i = j;
+                }
+            }
+            if (i != min_i) {
+                int tmp = numbers[i];
+                numbers[i] = numbers[min_i];
+                numbers[min_i] = tmp;
+            }
+        }
+        // Вывод результата на экран ПОСЛЕ того как отсортирован ВЕСЬ массив
+        for (int i = 0; i < numbers.length; i++)
+        {
+            System.out.println(numbers[i]);
+        }
+    }
+}
 
