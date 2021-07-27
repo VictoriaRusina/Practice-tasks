ca88772c7273d0a1e9aed43fda05d6fff40a0bba
VictoriaRusina <86785430+VictoriaRusina@users.noreply.github.com>
2021-07-27 11:28:18 +0300

    Update ArrayPractice7.java

--- ArrayPractice7.java
+++ ArrayPractice7.java
@@ -1,20 +1,31 @@
-//regular search = linear search
 import java.util.Arrays;
-public class ArrayPractice7
-{
-    public static void main(String[] args)
-    {
-        int[] numbers = {1, 4, 3, 2, 12};
-        int search = 12;
-
-        for (int index = 0; index < numbers.length; index++)
-        {
-            if (numbers[index] == search)
-            {
-                System.out.println(index);
-                break;
-            }
-
+public class ArrayFinal {
+    //Задание 2.1 a number to search in the array
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        int size = numbers.length;
+        System.out.println(size);
+        System.out.println("the first number is " + numbers[0]);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
         }
+        long startTime = System.nanoTime();
+        boolean findResult = linearSearch(numbers, 1);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        System.out.println(duration);
+        System.out.println(findResult);
+    }
+
+    public static boolean linearSearch(int[] numbers, int find) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == find) {
+                return true;
+            }
+        }
+        return false;
     }
 }
